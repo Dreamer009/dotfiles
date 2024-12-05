3f72a1f96717582020d4775154484336fbcedf88
Ben Sullivan <bls@me.com>
2024-12-04 18:47:34 -0600

    Add bat delta nushell nvim starship.toml and zed configurations

---
+++ nvim/init.lua
@@ -0,0 +1,17 @@
+require("ben.core")
+require("ben.lazy")
+
+-- local function open_nvim_tree(data)
+-- 	-- Ensure nvim-tree doesn't open if a directory is passed
+-- 	if vim.fn.isdirectory(data.file) == 1 then
+-- 		return
+-- 	end
+--
+-- 	-- Open nvim-tree and focus the file
+-- 	require("nvim-tree.api").tree.open()
+--
+-- 	-- Move cursor to the first open file
+-- 	vim.cmd("wincmd l")
+-- end
+--
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
