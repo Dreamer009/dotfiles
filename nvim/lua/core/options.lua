9775f3ade7d01222c0603e97ed750a178f38054c
Ben Sullivan <Dreamer009@users.noreply.github.com>
2024-12-08 15:47:40 -0600

    Add neovim spelling option and remove fugitive plugin

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -14,6 +14,9 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
+-- spelling asdf
+opt.spell = true
+
 -- line wrapping
 opt.wrap = false -- disable line wrapping
 
