bb7ca01ed523997b86adb6d75c289dee3bd94e9c
Ben Sullivan <Dreamer009@users.noreply.github.com>
2024-12-09 15:05:14 -0600

    Adds nvim noice plugin with lualine messages

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,44 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		-- "rcarriga/nvim-notify",
+	},
+	config = function()
+		require("telescope").load_extension("noice")
+
+		require("noice").setup({
+			views = {
+				mini = {
+					align = "message-left",
+					position = {
+						col = 0,
+					},
+					timeout = 5000,
+				},
+			},
+			-- Disable command-line messages (like `:w`, `:q`, etc.)
+			messages = {
+				enabled = true, -- enables the Noice messages UI
+				view = "mini", -- default view for messages
+				view_error = "mini", -- view for errors
+				view_warn = "mini", -- view for warnings
+				view_history = "messages", -- view for :messages
+				view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+			},
+			history = {
+				filter = {
+					["not"] = {
+						cmdline = false,
+						event = "msg_show",
+					},
+				},
+			},
+		})
+	end,
+}
