0cf8e23dae15d8b7e1bb450bf06dcc54c02ee14d
Ben Sullivan <Dreamer009@users.noreply.github.com>
2025-01-17 21:55:26 -0600

    Use Neovim mini-surround over nvim-surround plugin

--- nvim/lua/plugins/mini-surround.lua
+++ nvim/lua/plugins/mini-surround.lua
@@ -2,6 +2,22 @@ return {
   "echasnovski/mini.surround",
   version = "*",
   config = function()
-    require("mini.surround").setup()
+    require("mini.surround").setup({
+      mappings = {
+        add = "s", -- Add surrounding in Normal and Visual modes
+        delete = "ds", -- Delete surrounding
+        find = "fs", -- Find surrounding (to the right)
+        find_left = "Fs", -- Find surrounding (to the left)
+        highlight = "hs", -- Highlight surrounding
+        replace = "cs", -- Replace surrounding
+        update_n_lines = "sn", -- Update `n_lines`
+
+        suffix_last = "l", -- Suffix to search with "prev" method
+        suffix_next = "n", -- Suffix to search with "next" method
+      },
+
+      -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+      highlight_duration = 3000,
+    })
   end,
 }
