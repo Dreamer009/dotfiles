4bfa7c87ff5c107cc6da57496a77010dc5373e7b
Ben Sullivan <Dreamer009@users.noreply.github.com>
2025-12-14 20:08:05 -0600

    Remove trailing whitespace from files on save

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -53,3 +53,14 @@ opt.listchars = {
   tab = " ", -- Display tabs as "▸ " (customize as needed)
   trail = "", -- Display trailing spaces as "•"
 }
+
+-- Remove trailing whitespace on save for all files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+  desc = "Remove trailing whitespace on save",
+})
