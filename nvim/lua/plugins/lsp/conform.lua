41fa6af4e42572a33aab9e863cb5501daf478c7f
Ben Sullivan <Dreamer009@users.noreply.github.com>
2026-01-22 15:29:12 -0600

    Update rubocop conform formatter

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -10,6 +10,17 @@ return {
         lua = { "stylua" },
         ruby = { "rubocop" },
       },
+      formatters = {
+        rubocop = {
+          command = "sh",
+          args = {
+            "-c",
+            [[rubocop -a -f quiet --stderr --stdin "$0" 2>/dev/null | tail -n +2]],
+            "$FILENAME",
+          },
+          stdin = true,
+        },
+      },
       format_on_save = {
         lsp_fallback = true,
         async = false,
