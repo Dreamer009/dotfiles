18665099e520d49fd714460b1b2b16b487ba259c
Ben Sullivan <Dreamer009@users.noreply.github.com>
2026-04-17 23:03:36 -0500

    Fix nvim snack error

--- nvim/plugin/02-snacks.lua
+++ nvim/plugin/02-snacks.lua
@@ -9,7 +9,6 @@ snacks.setup({
     sections = {
       { section = "header" },
       { section = "keys", gap = 1, padding = 1 },
-      { section = "startup" },
     },
   },
   explorer = { enabled = true },
@@ -59,27 +58,53 @@ snacks.setup({
 local keymap = vim.keymap
 
 -- Snacks.lazygit
-keymap.set("n", "<leader>gb", function() Snacks.git.blame_line() end, { desc = "Git blame line" })
-keymap.set("n", "<leader>gl", function() Snacks.lazygit.log_file() end, { desc = "LazyGit Log File" })
-keymap.set("n", "<leader>lg", function() Snacks.lazygit() end, { desc = "LazyGit" })
+keymap.set("n", "<leader>gb", function()
+  Snacks.git.blame_line()
+end, { desc = "Git blame line" })
+keymap.set("n", "<leader>gl", function()
+  Snacks.lazygit.log_file()
+end, { desc = "LazyGit Log File" })
+keymap.set("n", "<leader>lg", function()
+  Snacks.lazygit()
+end, { desc = "LazyGit" })
 
 -- Snacks.explorer
-keymap.set("n", "<leader>e", function() Snacks.explorer.open({ hidden = true }) end, { desc = "Open Snacks.explorer" })
-keymap.set("n", "<C-k><C-b>", function() Snacks.explorer.open({ hidden = true }) end, { desc = "Open Snacks.explorer" })
+keymap.set("n", "<leader>e", function()
+  Snacks.explorer.open({ hidden = true })
+end, { desc = "Open Snacks.explorer" })
+keymap.set("n", "<C-k><C-b>", function()
+  Snacks.explorer.open({ hidden = true })
+end, { desc = "Open Snacks.explorer" })
 
 -- Snacks.notifier
-keymap.set("n", "<leader>fh", function() Snacks.notifier.show_history() end, { desc = "Find history" })
+keymap.set("n", "<leader>fh", function()
+  Snacks.notifier.show_history()
+end, { desc = "Find history" })
 
 -- Snacks.picker
-keymap.set("n", "<leader>ff", function() Snacks.picker.files({ hidden = true }) end, { desc = "Find files" })
-keymap.set("n", "<leader>fk", function() Snacks.picker.keymaps() end, { desc = "Find keymaps" })
-keymap.set("n", "<leader>fr", function() Snacks.picker.recent({ hidden = true }) end, { desc = "Find recent" })
-keymap.set("n", "<leader>fs", function() Snacks.picker.grep({ hidden = true }) end, { desc = "Find string" })
-keymap.set("n", "<leader>ft", function() Snacks.picker.todo_comments({ hidden = true }) end, { desc = "Find todo" })
+keymap.set("n", "<leader>ff", function()
+  Snacks.picker.files({ hidden = true })
+end, { desc = "Find files" })
+keymap.set("n", "<leader>fk", function()
+  Snacks.picker.keymaps()
+end, { desc = "Find keymaps" })
+keymap.set("n", "<leader>fr", function()
+  Snacks.picker.recent({ hidden = true })
+end, { desc = "Find recent" })
+keymap.set("n", "<leader>fs", function()
+  Snacks.picker.grep({ hidden = true })
+end, { desc = "Find string" })
+keymap.set("n", "<leader>ft", function()
+  Snacks.picker.todo_comments({ hidden = true })
+end, { desc = "Find todo" })
 
 -- Snacks.terminal
-keymap.set("n", "<leader>tt", function() Snacks.terminal.toggle() end, { desc = "Toggle terminal" })
-keymap.set("n", "<C-CR>", function() Snacks.terminal.toggle() end, { desc = "Toggle terminal" })
+keymap.set("n", "<leader>tt", function()
+  Snacks.terminal.toggle()
+end, { desc = "Toggle terminal" })
+keymap.set("n", "<C-CR>", function()
+  Snacks.terminal.toggle()
+end, { desc = "Toggle terminal" })
 
 -- Terminal buffer keymaps
 vim.api.nvim_create_autocmd("TermOpen", {
